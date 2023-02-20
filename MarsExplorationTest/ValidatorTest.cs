0f4a146ba7e9cf8950004ea9d2b8c613385e929e
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-02-20 14:26:57 +0100

    Create test project

---
+++ MarsExplorationTest/ValidatorTest.cs
@@ -0,0 +1,15 @@
+namespace MarsExplorationTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
