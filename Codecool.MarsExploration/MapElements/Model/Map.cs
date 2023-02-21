3325eaa77b07db86618f9fc666c5c4207e67f9a0
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-02-21 18:20:45 +0100

    Alter map representation

--- Codecool.MarsExploration/MapElements/Model/Map.cs
+++ Codecool.MarsExploration/MapElements/Model/Map.cs
@@ -8,6 +8,20 @@ public record Map(string?[,] Representation, bool SuccessfullyGenerated = false)
     public string CreateStringRepresentation(string?[,] arr)
     {
         return arr.Cast<string?>().Aggregate("", (output, line) => output + (line + "\n"));
+        // List<string> rows = new List<string>();
+        // for (int i = 0; i < arr.GetLength(0); i++)
+        // {
+        //     List<string> row = new List<string>();
+        //     for (int j = 0; j < arr.GetLength(1); j++)
+        //     {
+        //         row.Add(arr[i, j] ?? " ");
+        //         Console.Write(arr[i, j] + " ");
+        //     }
+        //
+        //     rows.Add(string.Join("", row));
+        // }
+        //
+        // return string.Join("\n", rows);
     }
 
     public override string ToString()
