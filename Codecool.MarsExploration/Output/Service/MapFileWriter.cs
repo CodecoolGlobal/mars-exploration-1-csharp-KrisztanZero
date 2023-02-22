dd65473dfb8e38117a54d1e5d084ac357fd5843a
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-02-22 14:21:30 +0100

    write map to file

---
+++ Codecool.MarsExploration/Output/Service/MapFileWriter.cs
@@ -0,0 +1,24 @@
+﻿using Codecool.MarsExploration.MapElements.Model;
+
+namespace Codecool.MarsExploration.Output.Service;
+
+public class MapFileWriter : IMapFileWriter
+{
+    public void WriteMapFile(Map map, string file)
+    {
+        using StreamWriter writer = new StreamWriter(file);
+
+        for (int i = 0; i < map.Representation.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.Representation.GetLength(1); j++)
+            {
+                string symbol = map.Representation[i, j] ?? " ";
+                writer.Write(symbol);
+            }
+
+            writer.WriteLine();
+        }
+    }
+}
+
+}
