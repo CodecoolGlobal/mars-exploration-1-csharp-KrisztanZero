04c58cd3b3f5c46b35ea730af96021a889632f00
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-02-22 13:58:02 +0100

    generate map

---
+++ Codecool.MarsExploration/MapElements/Service/Generator/MapGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Codecool.MarsExploration.Calculators.Model;
+using Codecool.MarsExploration.Calculators.Service;
+using Codecool.MarsExploration.Configuration.Model;
+using Codecool.MarsExploration.MapElements.Model;
+using Codecool.MarsExploration.MapElements.Service.Placer;
+
+namespace Codecool.MarsExploration.MapElements.Service.Generator;
+
+public class MapGenerator : IMapGenerator
+{
+    private readonly MapElementsGenerator _mapElementsGenerator = new();
+
+    private readonly MapElementPlacer _placer = new();
+    private readonly CoordinateCalculator _calculator = new();
+
+    public Map Generate(MapConfiguration mapConfig)
+    {
+        var mapElements = _mapElementsGenerator.CreateAll(mapConfig);
+        var mapSize = (int)(Math.Sqrt(mapConfig.MapSize));
+        var coordinate = _calculator.GetRandomCoordinate(mapSize);
+
+        string?[,] map = GetMap(mapConfig.MapSize);
+
+        foreach (var mapElement in mapElements)
+        {
+            if (_placer.CanPlaceElement(mapElement, map, coordinate))
+            {
+                _placer.PlaceElement(mapElement, map, coordinate);
+            }
+        }
+
+        return new Map(map);
+    }
+
+    private string[,] GetMap(int mapSize)
+    {
+        string[,] mapRepresentation = new string[(int)Math.Sqrt(mapSize), (int)Math.Sqrt(mapSize)];
+        for (int i = 0; i < Math.Sqrt(mapSize); i++)
+        {
+            for (int j = 0; j < Math.Sqrt(mapSize); j++)
+            {
+                mapRepresentation[i, j] = " ";
+            }
+        }
+
+        return mapRepresentation;
+    }
+}
