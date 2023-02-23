54cda1b1ce98086e496a06379a47dd6d3317df06
akosnandornagy <109482995+akosnandornagy@users.noreply.github.com>
2023-02-23 15:29:14 +0100

    Update MapElementPlacer.cs

--- Codecool.MarsExploration/MapElements/Service/Placer/MapElementPlacer.cs
+++ Codecool.MarsExploration/MapElements/Service/Placer/MapElementPlacer.cs
@@ -6,23 +6,23 @@ namespace Codecool.MarsExploration.MapElements.Service.Placer;
 
 public class MapElementPlacer : IMapElementPlacer
 {
-    // MapElement(string?[,] Representation, string Name, int Dimension, string? PreferredLocationSymbol = null) : Map(Representation)
-
     public bool CanPlaceElement(MapElement element, string?[,] map, Coordinate coordinate)
     {
-        // check if element is still on map
-        bool elementIsOnMap = coordinate.X + element.Dimension < map.GetLength(0) &&
-                              coordinate.Y + element.Dimension < map.GetLength(1);
+        var elementIsOnMap = coordinate.X + element.Dimension < map.GetLength(0) &&
+                             coordinate.Y + element.Dimension < map.GetLength(1);
         if (!elementIsOnMap) return false;
 
-        bool canPlaceElement = false;
+        var canPlaceElement = false;
 
-        CoordinateCalculator coordinateCalculator = new CoordinateCalculator();
+        var coordinateCalculator = new CoordinateCalculator();
         
         if (element.PreferredLocationSymbol != null)
         {
             var adjacentCoordinates = coordinateCalculator.GetAdjacentCoordinates(coordinate, 1).ToList();
-            foreach (var c in adjacentCoordinates.Where(c => c.X > map.GetLength(1) || c.Y > map.GetLength(0)))
+            
+            foreach (var c in adjacentCoordinates.Where(c =>
+                         c.X > map.GetLength(1) ||
+                         c.Y > map.GetLength(0)))
             {
                 adjacentCoordinates.Remove(c);
             }
@@ -30,49 +30,41 @@ public class MapElementPlacer : IMapElementPlacer
             switch (element.Name)
             {
                 case "mineral":
-                    foreach (var c in adjacentCoordinates)
+                    foreach (var c in adjacentCoordinates.Where(c =>
+                                 map[c.Y, c.X] == "#" &&
+                                 (map[coordinate.Y, coordinate.X] == " " || map[coordinate.Y, coordinate.X] == ".")))
                     {
-                        if (map[c.Y, c.X] == "#" && (map[coordinate.Y, coordinate.X] == " " || (map[coordinate.Y, coordinate.X] == ".")))
-                        {
-                            canPlaceElement = true;
-                        }
+                        canPlaceElement = true;
                     }
                     break;
                 case "water":
-                    foreach (var c in adjacentCoordinates)
+                    foreach (var c in adjacentCoordinates.Where(c =>
+                                 map[c.Y, c.X] == "&" &&
+                                 (map[coordinate.Y, coordinate.X] == " " || map[coordinate.Y, coordinate.X] == ".")))
                     {
-                        if (map[c.Y, c.X] == "&" && (map[coordinate.Y, coordinate.X] == " " || (map[coordinate.Y, coordinate.X] == ".")))
-                        {
-                            canPlaceElement = true;
-                        }
+                        canPlaceElement = true;
                     }
                     break;
                 default:
                     canPlaceElement = false;
                     break;
-                    
             }
         }
         else
         {
-            for (int i = 0; i < element.Dimension; i++)
+            for (var i = 0; i < element.Dimension; i++)
             {
                 for (var j = 0; j < element.Dimension; j++)
                 {
                     if (map[i + coordinate.Y, j + coordinate.X] != " ")
                     {
                         return false;
-                        
                     }
                     canPlaceElement = true;
                 }
-                
             }
-            
-            
         }
-
-
+        
         return canPlaceElement;
     }
 
